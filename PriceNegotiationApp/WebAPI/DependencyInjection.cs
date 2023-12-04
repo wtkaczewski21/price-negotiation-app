1da45dc29450c00ed03a0ea6c8a0190f2baf0579
wtkaczewski <wojtalokt@gmail.com>
2023-12-04 13:10:30 +0100

    create project structure

---
+++ PriceNegotiationApp/WebAPI/DependencyInjection.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace WebAPI;
+public static class DependencyInjection
+{
+    public static IServiceCollection AddWeb(this IServiceCollection services)
+    {
+        return services;
+    }
+}
