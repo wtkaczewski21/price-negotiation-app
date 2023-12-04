3918dd455007c3e2cb900fe983d69fe0023eda7e
wtkaczewski <wojtalokt@gmail.com>
2023-12-04 14:01:12 +0100

    feat: add product model

---
+++ PriceNegotiationApp/Domain/Models/Product.cs
@@ -0,0 +1,9 @@
+﻿namespace Domain.Models;
+
+public class Product
+{
+    public int Id { get; set; }
+    public required string Name { get; set; }
+    public string Description { get; set; } = string.Empty;
+    public decimal Price { get; set; }
+}
